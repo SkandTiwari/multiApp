ea5c7886807034ae38263c76fc52c7ddf2231ac2
SkandTiwari <tiwariskand11@gmail.com>
2021-09-02 15:57:53 +0530

    router commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import './components/Navbar.js'
 import Navbar from './components/Navbar.js';
+import Weather from './components/Weather.js';
+import Todo from './components/Todo';
+import Calculator from './components/Calculator';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
+      <>
+      <BrowserRouter>
         <Navbar/>
+        <Switch>
+        <Route path = "/weather" exact component = {Weather}/>
+        <Route path = "/todo" component = {Todo}/>
+        <Route path = "/calculator" component = {Calculator}/>
+        </Switch>
+      </BrowserRouter>
+      </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './navbar.css'
+import {Link} from 'react-router-dom'
+
 function Navbar() {
     return (
         <>
@@ -10,28 +12,34 @@ function Navbar() {
           </svg>
         </div>
 
-            <h1 className='logo-font'>ultiApp</h1>
+            <div className='logo-font'>ultiApp</div>
           </div> 
           <div class = 'navbar-bottom'>
-              <div className = 'app-card' id = 'weather-card'>
-              <svg xmlns="http://www.w3.org/2000/svg" width="65" height="65"  color = "#1451f7" fill="currentColor" class="bi bi-thermometer-sun" viewBox="0 0 16 16">
+            <Link to = "Weather">
+              <div className = 'app-card' id = 'weather-card' tabIndex = "1">
+                <svg xmlns="http://www.w3.org/2000/svg" width="65" height="65"  color = "#1451f7" fill="currentColor" class="bi bi-thermometer-sun" viewBox="0 0 16 16">
   <path d="M5 12.5a1.5 1.5 0 1 1-2-1.415V2.5a.5.5 0 0 1 1 0v8.585A1.5 1.5 0 0 1 5 12.5z"/>
   <path d="M1 2.5a2.5 2.5 0 0 1 5 0v7.55a3.5 3.5 0 1 1-5 0V2.5zM3.5 1A1.5 1.5 0 0 0 2 2.5v7.987l-.167.15a2.5 2.5 0 1 0 3.333 0L5 10.486V2.5A1.5 1.5 0 0 0 3.5 1zm5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-1 0v-1a.5.5 0 0 1 .5-.5zm4.243 1.757a.5.5 0 0 1 0 .707l-.707.708a.5.5 0 1 1-.708-.708l.708-.707a.5.5 0 0 1 .707 0zM8 5.5a.5.5 0 0 1 .5-.5 3 3 0 1 1 0 6 .5.5 0 0 1 0-1 2 2 0 0 0 0-4 .5.5 0 0 1-.5-.5zM12.5 8a.5.5 0 0 1 .5-.5h1a.5.5 0 1 1 0 1h-1a.5.5 0 0 1-.5-.5zm-1.172 2.828a.5.5 0 0 1 .708 0l.707.708a.5.5 0 0 1-.707.707l-.708-.707a.5.5 0 0 1 0-.708zM8.5 12a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-1 0v-1a.5.5 0 0 1 .5-.5z"/>
 </svg>
               </div>
-              <div className = 'app-card' id = 'todo-card'>
+              </Link>
+              <Link to = "Todo">
+              <div className = 'app-card' id = 'todo-card' tabIndex = "1">
               <svg xmlns="http://www.w3.org/2000/svg" width="65" height="65" color = "#1451f7" fill="currentColor" class="bi bi-list-task" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M2 2.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5V3a.5.5 0 0 0-.5-.5H2zM3 3H2v1h1V3z"/>
   <path d="M5 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM5.5 7a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9zm0 4a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9z"/>
   <path fill-rule="evenodd" d="M1.5 7a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5V7zM2 7h1v1H2V7zm0 3.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5H2zm1 .5H2v1h1v-1z"/>
 </svg>
               </div>
-              <div className = 'app-card' id = 'calc-card'>
+              </Link>
+              <Link to = "Calculator">
+              <div className = 'app-card' id = 'calc-card' tabIndex = "1">
               <svg xmlns="http://www.w3.org/2000/svg" width="65" height="65" color = "#1451f7" fill="currentColor" class="bi bi-calculator" viewBox="0 0 16 16">
   <path d="M12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h8zM4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4z"/>
   <path d="M4 2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5v-2zm0 4a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm3-6a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm3-6a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-4z"/>
 </svg>
               </div>
+              </Link>
 
           </div>  
         </>
